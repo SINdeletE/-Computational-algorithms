a1462306ee960871cac3651f0d77fa8e119eb08a
root <root@DESKTOP-K4FCDG7>
2025-02-19 23:44:28 +0300

    added lab_01 files

---
+++ lab_01/main.py
@@ -0,0 +1,4 @@
+import processing as pcs
+
+if __name__ == '__main__':
+    pcs.run()
---
+++ lab_01/processing.py
@@ -0,0 +1,19 @@
+def table_title_output():
+    x = "x"
+    y = "y"
+    y_der = "y_der"
+    y_der2 = "y_der2"
+    print(f"|{x:^15}|{y:^15}|{y_der:^15}|{y_der2:^15}|\n")
+
+def table_str_output(x: float, y: float, y_der: float, y_der2):
+    print(f"|{x:^15.2g}|{y:^15.4g}|{y_der:^15.4g}|{y_der2:^15.4g}|\n")
+
+def filedata_read(name: str):
+    return 
+
+def menu():
+    print("MENU\n")
+    print("1. Посчитать y(x) для заданного значения аргумента и ")
+
+def run():
+    
